3dcf89ff4a2b5aabd9576bd21b9773e63de12cdc
SL33PiNg <nook3920@gmail.com>
2025-03-16 09:56:49 +0700

    refactor: update index.vue layout and add navigation for drag and drop feature

--- Pages/index.vue
+++ Pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    index
-  </div>
+  <div class="d-flex flex-column justify-center align-center">
+    <p class="text-h2">Nuxt POC</p>
+    <div>
+      <p>Drag and Drop</p>
+      <NuxtLink to="/dnd2">Kanban</NuxtLink>
+    </div>
+</div>
 </template>
